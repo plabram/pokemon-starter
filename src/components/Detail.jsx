31944f74420b42417c6a476aeddf023a98295632
Penelope <freyalabram@gmail.com>
2023-12-13 13:43:57 +0100

    <Feat>Detail component calls pokemon from API

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { getPokemon } from '../api/pokemon'
+import Card from './Card'
 
 const Detail = () => {
   const { name } = useParams()
-  console.log(name)
+  const [pokemon, setPokemon] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  const getAndSetPokemon = async (name) => {
+    const selectedPokemon = await getPokemon(name)
+    setPokemon(selectedPokemon)
+    setLoading(false)
+    console.log(selectedPokemon)
+  }
+
+  useEffect(() => {
+    getAndSetPokemon(name)
+  }, [])
+
+  console.log(pokemon)
+
   return (
-    <div>
-      <div>{name}</div>
+    <div className="pokemon-detail">
+      {!loading ? <Card pokemon={pokemon} /> : null}
       <Link to={"/"}>Back to Home 🏡</Link>
     </div>
   )
