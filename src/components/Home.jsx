62f046a1ce7ad8229f4ff13ff8124390687b8fec
Penelope <freyalabram@gmail.com>
2023-12-13 13:12:35 +0100

    <Refactor>Move API call to new file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,29 +2,31 @@ import { useEffect, useState } from "react"
 import Card from './Card'
 import Filter from "./Filter"
 import { BottomOverlay, TopOverlay } from "./Overlay"
+import { getPokemon } from "../api/pokemon"
 
 function Home() {
 
   const [pokemons, setPokemons] = useState([{}])
   const [loading, setLoading] = useState(true)
-  const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
   const getPokemons = async () => {
     const pokemonsFromApi = []
     for (let id = 1; id <= 25; id++) {
-      try {
-        const response = await fetch(BASE_URL + id)
-        const pokemonAsJson = await response.json()
-        const newPokemon = {
-          id: pokemonAsJson.id,
-          name: pokemonAsJson.name,
-          image: pokemonAsJson.sprites.other["official-artwork"].front_shiny,
-          types: pokemonAsJson.types,
-        }
-        pokemonsFromApi.push(newPokemon)
-      } catch (error) {
-        console.log(error)
-      }
+      // try {
+      //   const response = await fetch(BASE_URL + id)
+      //   const pokemonAsJson = await response.json()
+      //   const newPokemon = {
+      //     id: pokemonAsJson.id,
+      //     name: pokemonAsJson.name,
+      //     image: pokemonAsJson.sprites.other["official-artwork"].front_shiny,
+      //     types: pokemonAsJson.types,
+      //   }
+      //   pokemonsFromApi.push(newPokemon)
+      // } catch (error) {
+      //   console.log(error)
+      // }
+      const newPokemon = await getPokemon(id)
+      pokemonsFromApi.push(newPokemon);
     }
     setPokemons(pokemonsFromApi)
     setLoading(false)
