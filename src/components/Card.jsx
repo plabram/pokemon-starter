91d74ef277f101acb733fb344091aa06f343e73e
Penelope <freyalabram@gmail.com>
2023-12-13 22:02:03 +0100

    <Feat>Polish styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,14 +26,19 @@ const Card = ({ pokemon }) => {
   return (
     <Link to={`/${name}`} >
       <div className='card-container'>
-        <div>
+        <div className="image-container">
           <span className="id">ID / {id}</span>
           <img src={image} alt={name} />
         </div>
-        <div>
+        <div className='text-container'>
           <h1>{capitaliseFirstLetter(name)}</h1>
           {types.map((type) => <span className="type" key={type.slot}>{type.type.name.toUpperCase()}</span>)}
-          {ascendant && !loading ? <p>Evoluciona de {ascendant}</p> : null}
+          {ascendant && !loading ?
+            <div className="evolved">
+              <p>Evoluciona de:</p>
+              <p className="ascendant">{ascendant}</p>
+            </div>
+            : null}
         </div>
       </div>
     </Link>
