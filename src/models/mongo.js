67b228493e93273d2f1985aedfef248c5afd4abe
Penelope <freyalabram@gmail.com>
2023-12-15 10:45:02 +0100

    <Refactor>Rename parent as ascendant git push

--- src/models/mongo.js
+++ src/models/mongo.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const pokemonSchema = new mongoose.Schema({
   name: { type: String, required: true },
   img: { type: String, required: true },
-  parent: [{ type: mongoose.Schema.Types.ObjectId, ref: "Pokemon" }],
+  ascendant: [{ type: mongoose.Schema.Types.ObjectId, ref: "Pokemon" }],
   types: [{ type: String, required: true }],
   friendlyId: { type: Number, required: true }, // Manually assigned user-friendly ID (for example, "1", "2", "35")
 });
